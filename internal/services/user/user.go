b54bea4ea43dd4b656f49a2770ffdb3e8c4358c7
m.vinnikov <m.vinnikov@yandex.ru>
2024-08-16 19:20:29 +0300

    refactoring user

--- internal/services/user/user.go
+++ internal/services/user/user.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"fmt"
+	"github.com/MagicNetLab/go-diploma/internal/services/logger"
 	"net/http"
 	"time"
 
@@ -19,20 +20,24 @@ type Claims struct {
 func Register(login string, password string) error {
 	isLoginExists, err := store.HasUserByLogin(login)
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed check register user login: %v", err))
 		return err
 	}
 
 	if isLoginExists {
+		logger.Info(fmt.Sprintf("failed register user: login %v already exists", login))
 		return ErrorUserExists
 	}
 
 	hashPass, err := encodePassword(password)
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed encode user password: %v", err))
 		return err
 	}
 
 	err = store.CreateUser(login, hashPass)
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed register user: %v", err))
 		return err
 	}
 
@@ -42,16 +47,19 @@ func Register(login string, password string) error {
 func Login(login string, password string) (string, error) {
 	u, err := store.GetUserByLogin(login)
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed get user by login to auth: %v", err))
 		return "", err
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed user login: compare password is fail. %v", err))
 		return "", err
 	}
 
 	token, err := generateToken(u.ID)
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed generate token: %v", err))
 		return "", err
 	}
 
@@ -66,6 +74,7 @@ func CheckAuthorize(r *http.Request) bool {
 
 	appConfig, err := config.GetAppConfig()
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed get app config: %v", err))
 		return false
 	}
 
@@ -79,6 +88,7 @@ func CheckAuthorize(r *http.Request) bool {
 			return []byte(appConfig.GetJWTSecret()), nil
 		})
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed parse token: %v", err))
 		return false
 	}
 
@@ -97,8 +107,10 @@ func GetAuthUserID(r *http.Request) (int, error) {
 
 	appConfig, err := config.GetAppConfig()
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed get app config: %v", err))
 		return 0, err
 	}
+
 	claims := &Claims{}
 	token, err := jwt.ParseWithClaims(cookie.Value, claims,
 		func(t *jwt.Token) (interface{}, error) {
@@ -109,8 +121,10 @@ func GetAuthUserID(r *http.Request) (int, error) {
 			return []byte(appConfig.GetJWTSecret()), nil
 		})
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed parse token: %v", err))
 		return 0, err
 	}
+
 	if !token.Valid || claims.UserID == 0 {
 		return 0, err
 	}
@@ -121,6 +135,7 @@ func GetAuthUserID(r *http.Request) (int, error) {
 func encodePassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed encode user password: %v", err))
 		return "", err
 	}
 
@@ -130,6 +145,7 @@ func encodePassword(password string) (string, error) {
 func generateToken(userID int) (string, error) {
 	cnf, err := config.GetAppConfig()
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed get app config: %v", err))
 		return "", err
 	}
 
@@ -142,6 +158,7 @@ func generateToken(userID int) (string, error) {
 
 	tokenString, err := token.SignedString([]byte(cnf.GetJWTSecret()))
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed generate token: %v", err))
 		return "", err
 	}
 
